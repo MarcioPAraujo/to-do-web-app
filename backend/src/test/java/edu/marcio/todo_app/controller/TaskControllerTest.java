b3a65fe6485ad363d4f651920bc83d18f8711166
Marcio <marciopereiraaraujo1997d1@gmail.com>
2026-04-04 11:13:21 -0300

    test: add edit controller tests

--- backend/src/test/java/edu/marcio/todo_app/controller/TaskControllerTest.java
+++ backend/src/test/java/edu/marcio/todo_app/controller/TaskControllerTest.java
@@ -3,6 +3,7 @@ package edu.marcio.todo_app.controller;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -16,6 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import edu.marcio.todo_app.dto.task.TaskResponse;
 import edu.marcio.todo_app.exception.task.DuplicatedNameException;
+import edu.marcio.todo_app.exception.task.NotFoundTaskException;
 import edu.marcio.todo_app.service.TaskService;
 
 @WebMvcTest(TaskController.class)
@@ -30,8 +32,12 @@ public class TaskControllerTest {
     return "/api/tasks";
   }
 
-  private String getJSON(String name) {
+  private String getRequestJSON(String name) {
     return String.format("{\"name\": \"%s\"}", name);
+  }
+
+  private String getEditRequestJSON(Long id, String name) {
+    return String.format("{\"id\": %d, \"name\": \"%s\"}", id, name);
   }
 
   @Test
@@ -63,8 +69,130 @@ public class TaskControllerTest {
 
     mockMvc.perform(post(baseURI())
         .contentType(MediaType.APPLICATION_JSON)
-        .content(getJSON("")))
+        .content(getRequestJSON("")))
         .andExpect(status().isBadRequest());
 
+  }
+
+  @Test
+  void shouldReturn400WhenCreateTaskWithNullName() throws Exception {
+    when(taskService.create(any())).thenThrow(new IllegalArgumentException("Task name cannot be blank"));
+
+    mockMvc.perform(post(baseURI())
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(getRequestJSON(null)))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  void shouldReturn400WhenCreateTaskWithNameOnlyHaveSpaces() throws Exception {
+    when(taskService.create(any())).thenThrow(new IllegalArgumentException("Task name cannot be blank"));
+
+    mockMvc.perform(post(baseURI())
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(getRequestJSON("   ")))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  void shouldReturn400WhenCreateTaskWithNameTooLong() throws Exception {
+    String longName = "a".repeat(256);
+    when(taskService.create(any()))
+        .thenThrow(new IllegalArgumentException("Task name cannot be longer than 255 characters"));
+
+    mockMvc.perform(post(baseURI())
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(getRequestJSON(longName)))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  void shouldReturn200WhenEditTask() throws Exception {
+    TaskResponse response = new TaskResponse(1L, "watch movie", false);
+    when(taskService.edit(any())).thenReturn(response);
+
+    mockMvc.perform(put(baseURI() + "/1")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(getEditRequestJSON(1L, "watch movie")))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.id").value(1L))
+        .andExpect(jsonPath("$.name").value("watch movie"))
+        .andExpect(jsonPath("$.completed").value(false));
+  }
+
+  @Test
+  void shouldReturn400WhenEditTaskWithBlankName() throws Exception {
+    when(taskService.edit(any())).thenThrow(new IllegalArgumentException("Task name cannot be blank"));
+
+    mockMvc.perform(put(baseURI() + "/1")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(getEditRequestJSON(1L, "")))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  void shouldReturn400WhenEditTaskWithNullName() throws Exception {
+    when(taskService.edit(any())).thenThrow(new IllegalArgumentException("Task name cannot be blank"));
+
+    mockMvc.perform(put(baseURI() + "/1")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(getEditRequestJSON(1L, null)))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  void shouldReturn400WhenEditTaskWithNameOnlyHaveSpaces() throws Exception {
+    when(taskService.edit(any())).thenThrow(new IllegalArgumentException("Task name cannot be blank"));
+
+    mockMvc.perform(put(baseURI() + "/1")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(getEditRequestJSON(1L, "   ")))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  void shouldReturn400WhenEditTaskWithNameTooLong() throws Exception {
+    String longName = "a".repeat(256);
+    when(taskService.edit(any()))
+        .thenThrow(new IllegalArgumentException("Task name cannot be longer than 255 characters"));
+
+    mockMvc.perform(put(baseURI() + "/1")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(getEditRequestJSON(1L, longName)))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  void shouldReturn409WhenEditTaskWithDuplicatedName() throws Exception {
+    when(taskService.edit(any())).thenThrow(new DuplicatedNameException("watch movie"));
+
+    mockMvc.perform(put(baseURI() + "/1")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(getEditRequestJSON(1L, "watch movie")))
+        .andExpect(status().isConflict());
+  }
+
+  @Test
+  void shouldReturn404WhenEditTaskThatDoesNotExist() throws Exception {
+    when(taskService.edit(any())).thenThrow(new NotFoundTaskException(999l));
+
+    mockMvc.perform(put(baseURI() + "/999")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(getEditRequestJSON(999L, "watch movie")))
+        .andExpect(status().isNotFound());
+  }
+
+  @Test
+  void shouldReturn200WhenNameKeepTheSameOnEdit() throws Exception {
+    TaskResponse response = new TaskResponse(1L, "watch movie", false);
+    when(taskService.edit(any())).thenReturn(response);
+
+    mockMvc.perform(put(baseURI() + "/1")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(getEditRequestJSON(1L, "watch movie")))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.id").value(1L))
+        .andExpect(jsonPath("$.name").value("watch movie"))
+        .andExpect(jsonPath("$.completed").value(false));
   }
 }
